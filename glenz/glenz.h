45cd5b05bce37d276b0a6ad950f5448190dca598
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-10-13 18:24:23 -0300

    Start glenz port
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ glenz/glenz.h
@@ -0,0 +1,8 @@
+#ifndef SR_GLENZ_H_
+#define SR_GLENZ_H_
+
+void cglenzdone(void);
+void zoomer2(void);
+void zoomer1(char *);
+
+#endif
---
+++ glenz/vec.h
@@ -0,0 +1,17 @@
+#ifndef SR_VEC_H_
+#define SR_VEC_H_
+
+int csetmatrix(int *,int,int,int);
+int crotlist(int *,int *);
+int cclipedges(int *,int *,int *); // modifies given point list
+int cprojlist(int *,int *);
+int ccliplist(int *);
+int cdrawpolylist(int *);
+int cmatrix_yxz(int,int,int,int *);
+int cpolylist(int *polylist,int *polys,int *edges,int *points3);
+int ceasypolylist(int *polylist,int *polys,int *points3);
+
+void cglenzpolylist(int *polylist);
+void cglenzinit(void);
+
+#endif
