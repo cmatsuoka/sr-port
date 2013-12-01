169f954e37144fe159f1526150fd7c6319f009a6
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-11-30 23:06:54 -0200

    Fix techno drawing to texture
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- techno/opengl.c
+++ techno/opengl.c
@@ -75,11 +75,13 @@ static const char fragment_shader_texture[] =
 
 
 static float tex_coords[] = {
-	0.0f, 0.5f,
-	1.0f, 0.5f,
+	0.0f, 1.0f,
+	1.0f, 1.0f,
 	0.0f, 0.0f,
 	1.0f, 0.0f
 };
+
+static int fb_location;
 
 
 static float color[256][4];
@@ -186,7 +188,7 @@ static void init_texture()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height;
-	unsigned char* image = SOIL_load_image("fc.png",
+	unsigned char* image = SOIL_load_image("sr-port.png",
 				&width, &height, 0, SOIL_LOAD_RGB);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
 				GL_UNSIGNED_BYTE, image);
@@ -208,6 +210,7 @@ int init_opengl()
 	view_width = 320;
 	view_height = 200;
 
+	glActiveTexture(GL_TEXTURE0);
 
 // The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
 glGenFramebuffers(1, &framebuffer);
@@ -215,19 +218,19 @@ glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 // The texture we're going to render to
 glGenTextures(1, &fb_texture);
 // "Bind" the newly created texture : all future texture functions will modify this texture
-glActiveTexture(GL_TEXTURE0);
 glBindTexture(GL_TEXTURE_2D, fb_texture);
+u2gl_set_tex_coords(tex_coords);
 // Give an empty image to OpenGL ( the last "0" )
 glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, window_width, window_height, 0,GL_RGB, GL_UNSIGNED_BYTE, 0);
 // Poor filtering. Needed !
 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 // Set "fb_texture" as our colour attachement #0
-glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, fb_texture, 0);
+glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fb_texture, 0);
 // Set the list of draw buffers.
-GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
-
+//GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+//glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
+	u2gl_check_error("framebuffer texture");
 
 
 	v = u2gl_compile_vertex_shader(vertex_shader);
@@ -237,6 +240,9 @@ glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
 	v = u2gl_compile_vertex_shader(vertex_shader_texture);
 	f = u2gl_compile_fragment_shader(fragment_shader_texture);
 	u2gl_create_program(&fc_program, f, v);
+
+	fb_location = glGetUniformLocation(fc_program.program, "uTexture");
+
 
 	glDisable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
@@ -253,8 +259,6 @@ glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
 	u2gl_set_matrix(&fc_program, m);
 
 	u2gl_check_error("init_opengl");
-
-	init_texture();
 
 	u2gl_projection(0, view_width, 0, view_height, &triangle_program);
 	u2gl_projection(0, view_width, 0, view_height, &fc_program);
@@ -274,5 +278,6 @@ void blend_color()
 
 void clear_screen()
 {
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
